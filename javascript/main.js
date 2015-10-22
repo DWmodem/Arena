a151016233f92cadc4fba054ca7c8674a4d2c9d4
Philippe Courtemanche <sigma.court@gmail.com>
2015-10-21 21:45:20 -0400

    mutated examples to have a dude in tilemap

--- javascript/main.js
+++ javascript/main.js
@@ -1,144 +1,211 @@
-//main
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gamebox', { preload: preload, create: create, update: update });
+// var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gamebox', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
-    game.load.image('sky', '../assets/sky.png');
-    game.load.image('ground', '../assets/platform.png');
+    game.load.image('ground_1x1', '../assets/ground_1x1.png');
     game.load.image('star', '../assets/star.png');
     game.load.spritesheet('dude', '../assets/dude.png', 32, 48);
 
 }
 
-var player;
-var platforms;
-var cursors;
+var map;
+var layer1;
+var layer2;
+var layer3;
 
-var stars;
-var score = 0;
-var scoreText;
+var marker;
+var currentTile = 0;
+var currentLayer;
+
+var cursors;
+var showLayersKey;
+var layer1Key;
+var layer2Key;
+var layer3Key;
 
 function create() {
 
-    //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    //  A simple background for our game
-    game.add.sprite(0, 0, 'sky');
-
-    //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = game.add.group();
-
-    //  We will enable physics for any object that is created in this group
     platforms.enableBody = true;
 
-    // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
+    game.stage.backgroundColor = '#2d2d2d';
 
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
+    player = game.add.sprite(0, 0, 'dude');
 
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
-
-    //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
-    ledge.body.immovable = true;
-
-    ledge = platforms.create(-150, 250, 'ground');
-    ledge.body.immovable = true;
-
-    // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
-
-    //  We need to enable physics on the player
     game.physics.arcade.enable(player);
-
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
     player.body.collideWorldBounds = true;
-
-    //  Our two animations, walking left and right.
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
-    //  Finally some stars to collect
-    stars = game.add.group();
 
-    //  We will enable physics for any star that is created in this group
-    stars.enableBody = true;
+    //  Creates a blank tilemap
+    map = game.add.tilemap();
 
-    //  Here we'll create 12 of them evenly spaced apart
-    for (var i = 0; i < 12; i++)
+    //  Add a Tileset image to the map
+    map.addTilesetImage('ground_1x1');
+
+    //  Creates a new blank layer and sets the map dimensions.
+    //  In this case the map is 40x30 tiles in size and the tiles are 32x32 pixels in size.
+    layer1 = map.create('level1',19 , 19, 32, 32);
+    layer1.scrollFactorX = 0.5;
+    layer1.scrollFactorY = 0.5;
+
+    //  Resize the world
+    layer1.resizeWorld();
+
+    layer2 = map.createBlankLayer('level2', 40, 30, 32, 32);
+    layer2.scrollFactorX = 0.8;
+    layer2.scrollFactorY = 0.8;
+
+    layer3 = map.createBlankLayer('level3', 40, 30, 32, 32);
+
+    currentLayer = layer3;
+
+    //  Create our tile selector at the top of the screen
+    createTileSelector();
+
+    game.input.addMoveCallback(updateMarker, this);
+
+    cursors = game.input.keyboard.createCursorKeys();
+
+    showLayersKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    layer1Key = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
+    layer2Key = game.input.keyboard.addKey(Phaser.Keyboard.TWO);
+    layer3Key = game.input.keyboard.addKey(Phaser.Keyboard.THREE);
+
+    showLayersKey.onDown.add(changeLayer, this);
+    layer1Key.onDown.add(changeLayer, this);
+    layer2Key.onDown.add(changeLayer, this);
+    layer3Key.onDown.add(changeLayer, this);
+
+    console.log(layer1.index);
+    console.log(layer2.index);
+    console.log(layer3.index);
+
+}
+
+function changeLayer(key) {
+
+    switch (key.keyCode)
     {
-        //  Create a star inside of the 'stars' group
-        var star = stars.create(i * 70, 0, 'star');
+        case Phaser.Keyboard.SPACEBAR:
+            layer1.alpha = 1;
+            layer2.alpha = 1;
+            layer3.alpha = 1;
+            break;
 
-        //  Let gravity do its thing
-        star.body.gravity.y = 300;
+        case Phaser.Keyboard.ONE:
+            currentLayer = layer1;
+            layer1.alpha = 1;
+            layer2.alpha = 0.2;
+            layer3.alpha = 0.2;
+            break;
 
-        //  This just gives each star a slightly random bounce value
-        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+        case Phaser.Keyboard.TWO:
+            currentLayer = layer2;
+            layer1.alpha = 0.2;
+            layer2.alpha = 1;
+            layer3.alpha = 0.2;
+            break;
+
+        case Phaser.Keyboard.THREE:
+            currentLayer = layer3;
+            layer1.alpha = 0.2;
+            layer2.alpha = 0.2;
+            layer3.alpha = 1;
+            break;
     }
 
-    //  The score
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+}
 
-    //  Our controls.
-    cursors = game.input.keyboard.createCursorKeys();
-    
+function pickTile(sprite, pointer) {
+
+    currentTile = game.math.snapToFloor(pointer.x, 32) / 32;
+
+}
+
+function updateMarker() {
+
+    marker.x = currentLayer.getTileX(game.input.activePointer.worldX) * 32;
+    marker.y = currentLayer.getTileY(game.input.activePointer.worldY) * 32;
+
+    if (game.input.mousePointer.isDown)
+    {
+        map.putTile(currentTile, currentLayer.getTileX(marker.x), currentLayer.getTileY(marker.y), currentLayer);
+        // map.fill(currentTile, currentLayer.getTileX(marker.x), currentLayer.getTileY(marker.y), 4, 4, currentLayer);
+    }
+
 }
 
 function update() {
-
-    //  Collide the player and the stars with the platforms
-    game.physics.arcade.collide(player, platforms);
-    game.physics.arcade.collide(stars, platforms);
-
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    game.physics.arcade.overlap(player, stars, collectStar, null, this);
-
+    var animation = 'none'
     //  Reset the players velocity (movement)
     player.body.velocity.x = 0;
+    player.body.velocity.y = 0;
 
     if (cursors.left.isDown)
     {
-        //  Move to the left
+        //Move to the left
         player.body.velocity.x = -150;
-
-        player.animations.play('left');
+        animation = 'left';
     }
-    else if (cursors.right.isDown)
+    if (cursors.right.isDown)
     {
-        //  Move to the right
+        //Move to the right
         player.body.velocity.x = 150;
-
-        player.animations.play('right');
+        animation = 'right';
     }
-    else
+    if (cursors.up.isDown)
     {
-        //  Stand still
-        player.animations.stop();
-
-        player.frame = 4;
+        //Move to the right
+        player.body.velocity.y = -150;
+    }
+    if (cursors.down.isDown)
+    {
+        //Move to the right
+        player.body.velocity.y = 150;
     }
     
     //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down)
+    if (animation == 'none')
     {
-        player.body.velocity.y = -350;
+        player.animations.stop()
+        player.frame = 4;
+    } else {
+        player.animations.play(animation);
     }
+}
+
+function render() {
+
+    game.debug.text('Current Layer: ' + currentLayer.name, 16, 550);
+    game.debug.text('1-3 Switch Layers. SPACE = Show All. Cursors = Move Dude', 16, 570);
 
 }
 
-function collectStar (player, star) {
-    
-    // Removes the star from the screen
-    star.kill();
+function createTileSelector() {
 
-    //  Add and update the score
-    score += 10;
-    scoreText.text = 'Score: ' + score;
+    //  Our tile selection window
+    var tileSelector = game.add.group();
+
+    var tileSelectorBackground = game.make.graphics();
+    tileSelectorBackground.beginFill(0x000000, 0.5);
+    tileSelectorBackground.drawRect(0, 0, 800, 34);
+    tileSelectorBackground.endFill();
+
+    tileSelector.add(tileSelectorBackground);
+
+    var tileStrip = tileSelector.create(1, 1, 'ground_1x1');
+    tileStrip.inputEnabled = true;
+    tileStrip.events.onInputDown.add(pickTile, this);
+
+    tileSelector.fixedToCamera = true;
+
+    //  Our painting marker
+    marker = game.add.graphics();
+    marker.lineStyle(2, 0x000000, 1);
+    marker.drawRect(0, 0, 32, 32);
 
 }
